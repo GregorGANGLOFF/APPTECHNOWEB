7ff9a3831b7d96bdbd61f2dbf5f77351fdc8b31d
GregorGANGLOFF <gregor.gangloff@isep.fr>
2016-06-22 15:40:36 +0200

    projet

---
+++ src/main/java/com/example/ApptechnowebApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptechnowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptechnowebApplication.class, args);
+	}
+}
